3796a54e3da685aeca7069007376e065f18abb39
nbenabla <77747926+nbenabla@users.noreply.github.com>
2021-12-05 18:32:45 -0500

    more preprocessing

--- data/preprocess.py
+++ data/preprocess.py
@@ -2,6 +2,11 @@ import json
 import pandas as pd
 import numpy as np
 import random
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import tensorflow as tf
+
+
 df = pd.read_csv("train_subset_emotion.csv", error_bad_lines=False, warn_bad_lines=False) 
 
 # if (utterance contains "hit:") delete row
@@ -9,10 +14,10 @@ df.drop(df[df['utterance'].str.contains("hit:")].index, inplace = True)
 
 
 
-emo=["neutral", "joy", "anger", "sadness", "fear"]
+# emo=["neutral", "joy", "anger", "sadness", "fear"]
 
-df["response_context"] = random.choices(emo, k=2854)
-df["context"] = random.choices(emo, k=2854)
+# df["response_context"] = random.choices(emo, k=2854)
+# df["context"] = random.choices(emo, k=2854)
 
 df["prompt_with_emotion"] = df["prompt"] + ", " + df["context"]
 df["response_with_emotion"] = df["utterance"] + ", " + df["response_context"]
@@ -37,18 +42,18 @@ parsed = json.loads(result)
 # "impressed", "caring", "devastated", "apprehensive", "disappointed"]
 
 
-emo_dict = { 0: 'neutral', 1: 'joy', 2: 'anger', 
-            3: 'sadness',4:'fear'}
+# emo_dict = { 0: 'neutral', 1: 'joy', 2: 'anger', 
+#             3: 'sadness',4:'fear'}
 
-emo2idx = {value:key for key,value in emo_dict.items()}
+# emo2idx = {value:key for key,value in emo_dict.items()}
 
 
-# emo_dict = {0:"sentimental", 1:"afraid", 2:"proud", 3:"faithful", 4:"terrified", 5:"angry", 6:"sad", 7:"joyful", 8:"prepared" , 9:"embarrased", 10:"annoyed", 11:"lonely", 
-# 12:"ashamed", 13:"guilty", 14:"surprised", 15:"furious", 16:"disgusted", 17:"hopeful", 18:"confident", 19:"excited", 20:"nostalgic", 21:"grateful", 22:"anticipating", 23:"jealous",
-# 24:"impressed", 25:"caring", 26:"devastated", 27:"apprehensive", 28:"disappointed", 29:"anxious", 30:"embarrassed", 31:"content",
-# 32:"content", 33:"trusting"}
+emo_dict = {0:"sentimental", 1:"afraid", 2:"proud", 3:"faithful", 4:"terrified", 5:"angry", 6:"sad", 7:"joyful", 8:"prepared" , 9:"embarrased", 10:"annoyed", 11:"lonely", 
+12:"ashamed", 13:"guilty", 14:"surprised", 15:"furious", 16:"disgusted", 17:"hopeful", 18:"confident", 19:"excited", 20:"nostalgic", 21:"grateful", 22:"anticipating", 23:"jealous",
+24:"impressed", 25:"caring", 26:"devastated", 27:"apprehensive", 28:"disappointed", 29:"anxious", 30:"embarrassed", 31:"content",
+32:"content", 33:"trusting"}
 
-# emo2idx = {v: k for k, v in emo_dict.items()}
+emo2idx = {v: k for k, v in emo_dict.items()}
 
 lst = parsed['data']
 output = []
@@ -73,6 +78,7 @@ word_len = 100
 vocab = []
 for i in output:
     question = i[0][0]
+    question.replace("'", " ")
     if (len(question) > 1):
     # only get first 10 words, get rid of rest
         split1 = question.split(" ")[:word_len]
@@ -82,6 +88,7 @@ for i in output:
         vocab.append(["SOS"] + split1 + ["EOS"])
         question_emo=i[0][1]
         answer = i[1][0][0]
+        answer.replace("'", " ")
         split2 = answer.split(" ")[:word_len]
         if len(split2) < word_len:
             for y in range(word_len-len(split2)):
@@ -93,8 +100,8 @@ for i in output:
         for j in range(1,len(i[1])):
             if j<len(i[1]) -1:
                 pairs.append([i[1][j][0], i[1][j+1][0]])
-                split3 = i[1][j][0].split(" ")[:word_len]
-                split4 = i[1][j+1][0].split(" ")[:word_len]
+                split3 = i[1][j][0].replace("'", " ").split(" ")[:word_len]
+                split4 = i[1][j+1][0].replace("'", " ").split(" ")[:word_len]
                 if len(split3) < word_len:
                     for w in range(word_len-len(split3)):
                         split3.append("PAD")
@@ -112,6 +119,9 @@ for i, sentence in enumerate(vocab):
     for word in sentence:
         word = word.replace("_comma_", "").replace("!", "").replace("?", "").replace(".", "").replace("..", "").replace("...", "").lower().strip()
         word = ''.join([k for k in word if not k.isdigit()]).replace(",", "").replace("$", "").replace("-", "")
+        word2index[word] = i
+        index2word[i] = word
+
 
 
 for i in pairs:
@@ -119,11 +129,6 @@ for i in pairs:
         i[word] = i[word].replace("_comma_", "").replace("!", "").replace("?", "").replace(".", "").replace("..", "").replace("...", "").lower().strip()
         i[word] = ''.join([k for k in i[word] if not k.isdigit()]).replace(",", "").replace("$", "").replace("-", "")
         
-    
-
-
-
-
 class Voc:
     def __init__(self):
         self.name = 'train'
@@ -182,9 +187,9 @@ def binaryMatrix(l, value=PAD_token):
 # Returns padded input sequence tensor and lengths
 def inputVar(l, voc):
     indexes_batch = [indexesFromSentence(voc, sentence) for sentence in l]
-    lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
+    lengths = tf.convert_to_tensor([len(indexes) for indexes in indexes_batch], dtype=tf.int32)
     padList = zeroPadding(indexes_batch)
-    padVar = torch.LongTensor(padList)
+    padVar = tf.convert_to_tensor(padList, dtype=tf.int32)
     return padVar, lengths
 
 # Returns padded target sequence tensor, padding mask, and max target length
@@ -194,11 +199,13 @@ def outputVar(l, voc):
     padList = zeroPadding(indexes_batch)
     mask = binaryMatrix(padList)
     mask = torch.ByteTensor(mask)
-    padVar = torch.LongTensor(padList)
+    mask = tf.convert_to_tensor(mask.numpy())
+    padVar = tf.convert_to_tensor(padList, dtype=tf.int32)
     return padVar, mask, max_target_len
 # return an emotion tensor
 def emotion_tensor(input_list):
-    return torch.LongTensor(input_list)
+    # return torch.LongTensor(input_list)
+    return tf.convert_to_tensor(input_list, dtype=tf.int32)
 
 def batch2TrainData(voc, index,pairs,pairs_emotion):
     pair_batch = [pairs[idx] for idx in index]
